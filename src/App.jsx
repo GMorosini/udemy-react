86d566116f482dc12c170fc0901f5a3daedd6f28
GMorosini <gmorosini88@gmail.com>
2025-05-14 10:44:38 -0300

    Criando componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+
+import Welcome from './components/Welcome';
+import BomDia from './components/BomDia';
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <h1>Olá Mundo! Bora aprender React!</h1>
-    </>
+  return( <>
+            <Welcome />
+            <BomDia />
+          </> 
   );
 }
 
---
+++ src/components/BomDia.jsx
@@ -0,0 +1,6 @@
+function BomDia(){
+    const name = "Gabriel Morosini";
+    return( <div>Bom dia {name }</div> );
+}
+
+export default BomDia;
